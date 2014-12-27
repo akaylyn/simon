9da698d255ecdf0a772e820942f608f593b5a357
the-magister <magister@the-magister.com>
2014-12-27 13:48:09 -0800

    Stubbed in some Extern work to simply listen for towerInstruction.
    Easiest external case to handle: the extern device sends towerInstruction
    packets, indicating they want the Towers to do some stuff.  The Console
    notes this, sets a 10 second timer to prevent gamePlay from happending,
    and plays the towerInstruction.lightLevel as button presses on the Console.
    fireLevel is ignored, but whatever.  The Towers, of course, do play out
    the fireLevel.

--- src/Console/Extern.cpp
+++ src/Console/Extern.cpp
@@ -1,16 +1,61 @@
 #include "Extern.h"
 
+// this is where the lights and fire instructions to Towers are placed
+// defined in Simon_Comms.h and instantiated in Tower.cpp
+extern towerInstruction inst;
+// from Simon_Comms.h:
+// typedef struct {
+//	byte lightLevel[N_COLORS]; // 0..255.  maps to analogWrite->light level
+//	byte fireLevel[N_COLORS]; // 0..255.  maps to timer->fire duration
+// } towerInstruction;
+// structure stores how Towers acts on those towerInstructions
+
+// Need an instance of the Radio Module.  Instantiated in Tower.cpp.
+extern RFM12B radio;
+
 // sets up external interface
 void externStart() {
   Serial << F("Extern: startup.") << endl;
-  
+
   // nothing to do, yet.
 }
 
 // checks for external traffic; if traffic, responds to it and returns true
 boolean externUpdate() {
-  // nothing to do, yet.
+  // once we get radio comms, wait this long  before returning false from externUpdate.
+  static Metro externTimeout(EXTERNAL_COMMS_TIMEOUT);
 
-  return( false );
+  // important that we check the buffer before sending, or we'll overwrite stuff
+  if ( radio.ReceiveComplete() && radio.CRCPass() ) {
+    // we have radio comms
+    externTimeout.reset();
+
+    // check cases
+    if ( radio.GetDataLen() == sizeof(inst) ) {
+
+      // save instruction for lights/flame
+      inst = *(towerInstruction*)radio.GetData();
+      // do it.
+      externDoInstruction();
+
+    }
+
+    // check for ACK request
+    if ( radio.ACKRequested() )
+      radio.SendACK();
+
+  }
+
+  return ( !externTimeout.check() );
 }
 
+// takes a towerInstruction and plays that out on the Console.  meh.
+void externDoInstruction() {
+  // very straight-forward.  Emulates button presses on the Console according to towerInstruction light settings.
+  pixelSet(I_RED, inst.lightLevel[I_RED]);
+  pixelSet(I_GRN, inst.lightLevel[I_GRN]);
+  pixelSet(I_BLU, inst.lightLevel[I_BLU]);
+  pixelSet(I_YEL, inst.lightLevel[I_YEL]);
+
+  // towerInstruction fireLevel is ignored on the Console.
+}
--- src/Console/Extern.h
+++ src/Console/Extern.h
@@ -6,11 +6,22 @@
 #include <Arduino.h>
 
 #include <Streaming.h> // <<-style printing
+#include <Metro.h> // timers.
+#include <Simon_Comms.h> // towerInstruction definition.
+#include <Simon_Indexes.h> // indexing.
+
+// once we get radio comms, wait this long  before returning false from externUpdate.
+#define EXTERNAL_COMMS_TIMEOUT 10000UL
 
 // sets up external interface
 void externStart();
 
 // checks for external traffic; if traffic, responds to it and returns true
 boolean externUpdate();
+
+// takes a towerInstruction and plays that out on the Console.  meh.
+void externDoInstruction();
+// set pixel light (or I_ALL) to a value.  Defined in Light.cpp.
+extern void pixelSet(byte lightIndex, byte pixelValue);
 
 #endif
