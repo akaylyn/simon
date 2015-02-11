77b7d3a4bfd30622b41decea122fbb8f4d773fbf
the-magister <magister@the-magister.com>
2015-02-11 12:57:31 -0800

    Found some more IR codes (44-key).

--- tests/Tower/IR_test/IR_test.ino
+++ tests/Tower/IR_test/IR_test.ino
@@ -38,6 +38,126 @@ IRsend irsend;
 #define BRIGTH_STEP        10
 #define CODE_REPEAT        0xFFFFFF
 
+/* from http://forum.osmc.tv/showthread.php?tid=7142
+
+      led-brighter            0x3AC5
+          led-dim               0xBA45
+          led-power             0x02FD
+          
+          
+          led-red               0x1AE5
+          led-green             0x9A65
+          led-blue              0xA25D
+          led-white        0x22DD
+          
+          
+          led-redorange        0x2AD5
+          led-warmwhite        0x12ED
+          
+
+          led-orange        0x0AF5
+          led-pink        0x32CD
+          
+
+          led-yellow        0x38C7
+          led-cyan        0xB847
+          led-lightpurple    0x7887
+          led-turquoise        0xF807
+          
+
+          led-greenyellow    0x18E7
+          led-babyblue        0x9867
+          led-purple        0x58A7
+          led-bluewhite        0xD827          
+          
+
+          led-redup             0x28D7
+          led-greenup           0xA857
+          led-blueup            0x6897
+          led-faster            0xE817
+          
+
+          led-reddown           0x08F7
+          led-greendown         0x8877
+          led-bluedown          0x48B7
+          led-slower            0xC837
+          
+
+          led-diy1              0x30CF
+          led-diy2              0xB04F
+          led-diy3              0x708F
+          led-auto         0xF00F
+          
+
+          led-diy4              0x10EF
+          led-diy5              0x906F
+          led-diy6              0x50AF
+          led-flash        0xD02F
+          
+
+          led-jump3        0x20DF
+          led-jump7        0xA05F
+          led-fade3        0x609F
+          led-fade7        0xE01F
+          
+*/
+/* or: for a 44-key remote:
+          LED_UP                0x3AC5
+          LED_DOWN              0xBA45
+          LED_Power_ON          0x827D
+          LED_Power_OFF         0x02FD
+
+          LED_RED               0x1AE5
+          LED_GREEN             0x9A65
+          LED_BLUE              0xA25D
+          LED_WHITE             0x22DD
+
+          LED_ORANGE            0x2AD5
+          LED_PEE-GREEN         0xAA55 
+          LED_DARK-BLUE         0x926D
+          LED_MILK-WHITE        0x12ED
+
+          LED_DARK-YELLOW       0x0AF5
+          LED_CYAN              0x8A75 
+          LED_LYON-BLUE         0xB24D
+          LED_WHITE-PINK        0x32CD
+
+          LED_YELLOW            0x38C7
+          LED_LIGHT-BLUE        0xB847
+          LED_PINK              0x7887
+          LED_GREEN-WHITE       0xF807
+
+          LED_LIGHT-YELLOW      0x18E7
+          LED_SKY-BLUE          0x9867
+          LED_BROWN             0x58A7
+          LED_BLUE-WHITE        0xD827
+
+          RED_UP                0x28D7
+          GREEN_UP              0xA857
+          BLUE_UP               0x6897
+          QUICKER               0xE817
+
+          RED_DOWN              0x08F7
+          GREEN_DOWN            0x8877
+          BLUE_DOWN             0x48B7
+          SLOWER                0xC837
+
+          DIY1                  0x30CF
+          DIY2                  0xB04F
+          DIY3                  0x708F
+          AUTO                  0xF00F
+
+          DIY4                  0x10EF
+          DIY5                  0x906F
+          DIY6                  0x50AF
+          FLASH                 0xD02F
+
+          JUMP3                 0x20DF
+          JUMP7                 0xA05F
+          FADE3                 0x609F
+          FADE7                 0xE01F
+
+*/
 void setup()
 {
   Serial.begin(115200);
