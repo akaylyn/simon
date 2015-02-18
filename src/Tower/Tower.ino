72639460862d08f1a0cd1192aead1979cd58f0e3
the-magister <magister@the-magister.com>
2015-02-18 12:09:12 -0800

    Closing the solenoid is going to generate a EMI spike, which will make sensors suspect.  Added code to ignore sensors for a period of time after the solenoid closes.
    Also added some gamma correction to the LED strips to get linear luminosity.

--- src/Tower/Tower.ino
+++ src/Tower/Tower.ino
@@ -15,12 +15,6 @@ towerInstruction inst;
 // structure stores how this Tower acts on those towerInstructions
 towerConfiguration config;
 
-// for yellow on a RGB strip, we need to active red and green in some proportion.
-// from testing, the red should be 74% and green 26%.
-// So, if run the red strip at the luminance requested
-// for yellow, then the green strip needs to be 0.35 = the ratio 0.26/0.74.
-const byte greenYellowFraction = 35;
-
 // Need an instance of the Radio Module
 RFM69 radio;
 // store my NODEID
@@ -35,30 +29,23 @@ boolean networkTimedOut = true; // flip this false with traffic.
 // track how long we need to have the flame on for.
 Metro flameOnTime(100UL);
 // but only fire this often.
-Metro flameCoolDownTime(2000UL);
+Metro flameCoolDownTime(1000UL);
+// there's quite a bit of EMI from the solenoid firing, so we add a cooldown on reading sensors
+Metro sensorEmiCooldown(1000UL);
 
 // when we're out of the network, update the lights on this interval (approx)
 #define SOLO_TEST_PATTERN_UPDATE 3000UL // ms
 
 // when we're in the network and idle, update the lights on this interval
 #define NETWORK_TEST_PATTERN_UPDATE 20UL // ms
-Metro boredUpdateInterval(NETWORK_TEST_PATTERN_UPDATE);
 
 // pin locations for outputs
 #define LED_R 3 // the PWM pin which drives the red LED
 #define LED_G 5 // the PWM pin which drives the green LED
 #define LED_B 6 // the PWM pin which drives the blue LED
-#define LED_W 9 // the PWM pin which drives the blue LED
-#define FLAME 7 // JeeNode Port 1, pin 2 (DIO)
-#define OTHER 8 // JeeNode Port 1, pin 5 (AIO)
-
-// minimum value on the LEDs that don't flicker
-#define MIN_PWM 3
-
-// luminosity is not equal among LEDs
-#define RED_MAX 255
-#define GRN_MAX 220
-#define BLU_MAX 210
+#define LED_W 9 // the PWM pin which drives the white LED (not connected)
+#define FLAME 7 // relay for solenoid
+#define OTHER 8 // relay (not connected)
 
 // remote control
 #define RESET_PIN A0
@@ -66,19 +53,11 @@ Metro boredUpdateInterval(NETWORK_TEST_PATTERN_UPDATE);
 #define DEBOUNCE_TIME 50UL // need a long debounce b/c electrical noise from solenoid.
 Bounce systemReset = Bounce(RESET_PIN, DEBOUNCE_TIME);
 Bounce gameEnable = Bounce(GAME_ENABLE_PIN, DEBOUNCE_TIME);
-// track states here
-#define SETUP_LIGHT_CYCLE_INTERVAL 30000UL
-Metro setupModeLightTimer(SETUP_LIGHT_CYCLE_INTERVAL);
 
-boolean systemReseted() {
-  // at system power up, relay is open, meaning pin will read HIGH.
-  return ( systemReset.read() == LOW );
-}
-
-boolean gameEnabled() {
-  // at system power up, relay is open, meaning pin will read HIGH.
-  return ( gameEnable.read() == HIGH );
-}
+// luminosity is not equal among LEDs
+#define RED_MAX 255 // entry 255 in the gamma lookup
+#define GRN_MAX 220 // entry 242 in the gamma lookup
+#define BLU_MAX 210 // entry 238 in the gamma lookup
 
 void setup() {
   // put your setup code here, to run once:
@@ -88,7 +67,7 @@ void setup() {
   randomSeed(analogRead(A3)); // or some other unconected pin
 
   // see: http://jeelabs.org/2011/11/09/fixing-the-arduinos-pwm-2/
-  bitSet(TCCR1B, WGM12);
+  bitSet(TCCR1B, WGM12); // puts Timer1 in Fast PWM mode to match Timer0.
 
   Serial << F("Configuring output pins.") << endl;
   flameOff(); pinMode(FLAME, OUTPUT); // order is important.  set then output.
@@ -127,15 +106,13 @@ void setup() {
   // get last configuration
   commsLoad(config);
   commsPrint(config, myNodeID);
+  
   // set flame cooldown from config
   flameCoolDownTime.interval(config.flameCoolDownTime);
 
-  // initialize instructions (i.e. none).
-  commsDefault(inst);
-  performInstruction();
+  // initialize with lighting on.
+  whiteTestPattern();
   
-  // ramp up lights.
-  lightsAllOn();  
 }
 
 
@@ -143,15 +120,25 @@ void loop() {
   // SAFETY: do not move this code after any other code.
   if ( flameOnTime.check() ) { // time to turn the flame off
     flameOff();
+    sensorEmiCooldown.reset(); // sensing needs to pause.
   }
 
-  // check system enable state
-  if ( systemReset.update() ) { // reset state change
-    flameOff(); // just in case.  note that the while() loop prevents flameOnTime.check(), above.
-    while ( systemReseted() ) {
-      // in RST mode.
-      // ramp the lights
-      updateResetTestPattern();
+  static boolean gameEnableFlag = gameEnabled();
+  static boolean systemResetFlag = systemReseted();
+  
+  // put all sensor code inside of this cooldown check.
+  if( sensorEmiCooldown.check() ) {
+    // check system enable state
+    if ( systemReset.update() ) { // reset state change
+      Serial << F("Reset state change.  State: ");
+      systemResetFlag = systemReseted();
+      Serial << systemResetFlag << endl;
+    }
+    // check system enable state
+    if ( gameEnable.update() ) { // gameplay enable state change
+      Serial << F("Gameplay state change.  State: ");
+      gameEnableFlag = gameEnabled();
+      Serial << gameEnableFlag << endl;
     }
   }
 
@@ -190,9 +177,6 @@ void loop() {
       // show it
       commsPrint(config, myNodeID);
 
-      // set flame cooldown from config
-      flameCoolDownTime.interval(config.flameCoolDownTime);
-
       // note we're configured
       amConfigured = true;
     }
@@ -202,57 +186,53 @@ void loop() {
     }
   }
 
-  // check system enable state
-  static boolean gameEnableFlag = gameEnabled();
-  if ( gameEnable.update() ) { // gameplay enable state change
-    Serial << F("Gameplay state change.  State: ");
-    gameEnableFlag = gameEnabled();
-    Serial << gameEnableFlag << endl;
-  }
-  
   if ( networkTimeout.check() ) networkTimedOut = true;
-  
-  if ( networkTimedOut ) { // if the comms are quiet
-    // run some test patterns/attractants
-    if ( gameEnableFlag ) {
-      // the game's afoot.
-      if ( amConfigured ) {
-        updateNetworkTestPattern(); // breathing pattern, indicates configuration received from Console
-      } else { 
-        updateSoloTestPattern(); // rotating lights, indicates configuration not received from Console (yet?)    
-      }
+
+  if ( systemResetFlag ) {
+    // if the reset pin is held, turn to red lighting
+    redTestPattern();
+  } else if ( networkTimedOut ) {
+    // comms are quiet, so some test patterns are appropriate
+    if( !gameEnableFlag ) {
+      // if the gameplay pin is held, use white lighting after comms timeout
+      whiteTestPattern();
+    } else if( amConfigured ) {
+      // if we've gotten configuration from Console and comms timeout, cycle the lights
+      inNetworkTestPattern(); // breathing pattern, indicates configuration received from Console
     } else {
-      // not in gameplay mode, bring up the lights
-      lightsAllOn();
-       // ramp the lights peridically
-//      if ( setupModeLightTimer.check() ) {
-//        Serial << F("Not in Gameplay mode.  Ramping lights...") << endl;
-//        updateResetTestPattern();
-//        setupModeLightTimer.reset();
-//      }
+      // if we've not gotten configuration from Console and comms timeout, flip the lights
+      soloTestPattern();
     }
   }
 
 }
 
-void lightsAllOn() {
-   lightSet(LED_R, RED_MAX/2);
-   lightSet(LED_G, GRN_MAX/2);
-   lightSet(LED_B, BLU_MAX/2);
+boolean systemReseted() {
+  // at system power up, relay is open, meaning pin will read HIGH.
+  return ( systemReset.read() == LOW );
 }
 
-void updateResetTestPattern() {
-  // ramp up lights to white quickly.
-  for (byte i = 0; i < 255; i++) {
-    // set them, apping a final scaling to keep brightness roughly equivalent.
-    lightSet(LED_R, map(i, 0, 255, 0, RED_MAX/2));
-    lightSet(LED_G, map(i, 0, 255, 0, GRN_MAX/2));
-    lightSet(LED_B, map(i, 0, 255, 0, BLU_MAX/2));
-    delay(5);
-    // check for state changes.
-    systemReset.update();
-    gameEnable.update();
-  }
+boolean gameEnabled() {
+  // at system power up, relay is open, meaning pin will read HIGH.
+  return ( gameEnable.read() == HIGH );
+}
+
+void whiteTestPattern() {
+  commsDefault(inst);
+
+  inst.lightLevel[I_RED] = 128;
+  inst.lightLevel[I_GRN] = 128;
+  inst.lightLevel[I_BLU] = 128;
+
+  performInstruction();
+}
+
+void redTestPattern() {
+  commsDefault(inst);
+
+  inst.lightLevel[I_RED] = 255;
+  
+  performInstruction();  
 }
 
 // generate a random number on the interval [a, b] with mode c.
@@ -269,13 +249,9 @@ long trandom(long a, long c, long b) {
 
 }
 
-void updateSoloTestPattern() {
-
-  // pin locations
-  static int pins[3] = {LED_R, LED_G, LED_B};
-
+void soloTestPattern() {
   // gotta start somewhere
-  static byte currInd = N_COLORS;
+  static byte currInd = random(0, N_COLORS);
   // track the time
   static Metro onTime(SOLO_TEST_PATTERN_UPDATE);
 
@@ -283,21 +259,22 @@ void updateSoloTestPattern() {
     // set to zero.
     commsDefault(inst);
 
-    currInd = ++currInd % 4; // select the next color
+    currInd = random(0, N_COLORS); // select the next color
     Serial << F("Test pattern (not configured).  Color: ") << currInd << endl;;
 
     inst.lightLevel[currInd] = 255;
     performInstruction();
 
     // reset the interval update to add some randomness.
-    onTime.interval(trandom(SOLO_TEST_PATTERN_UPDATE / 2, SOLO_TEST_PATTERN_UPDATE, SOLO_TEST_PATTERN_UPDATE * 2));
+    onTime.interval(random(SOLO_TEST_PATTERN_UPDATE/2, SOLO_TEST_PATTERN_UPDATE/2 + 1));
     onTime.reset();
   }
 
 }
 
 // update lighting on an interval when there's nobody playing
-void updateNetworkTestPattern() {
+void inNetworkTestPattern() {
+  static Metro boredUpdateInterval(NETWORK_TEST_PATTERN_UPDATE);
   // only update on an interval, or we're spamming the comms and using up a lot of CPU power with float operations
   if ( !boredUpdateInterval.check() ) return;
   boredUpdateInterval.reset(); // for next loop
@@ -313,7 +290,7 @@ void updateNetworkTestPattern() {
 
   if ( cycleTimer.check() ) { // enter a new cycle    
     currInd = random(0, N_COLORS);
-    cycleTime = random(1000, 7001);
+    cycleTime = random(3000, 10001);
     Serial << F("Test pattern (configured).  Color: ") << currInd << F(" cycle time: ") << cycleTime << endl;
     cycleTimer.interval(cycleTime);
     cycleTimer.reset();
@@ -326,7 +303,7 @@ void updateNetworkTestPattern() {
   // start 3/4 phase out, at zero
   inst.lightLevel[currInd] = (exp(sin(ts + 0.75 * 2.0 * PI)) - 0.36787944) * 108.0;
   
-  Serial << currInd << ": " << ts << " " << inst.lightLevel[currInd] << endl;
+//  Serial << currInd << ": " << ts << " " << inst.lightLevel[currInd] << endl;
   
 //  inst.lightLevel[I_RED] = (exp(sin(ts + 0.00 * 2.0 * PI)) - 0.36787944) * 108.0;
 //  inst.lightLevel[I_GRN] = (exp(sin(ts + 0.25 * 2.0 * PI)) - 0.36787944) * 108.0;
@@ -339,6 +316,12 @@ void updateNetworkTestPattern() {
 
 void performInstruction() {
   // process inst through the filter of config
+
+  // for yellow on a RGB strip, we need to active red and green in some proportion.
+  // from testing, the red should be 74% and green 26%.
+  // So, if run the red strip at the luminance requested
+  // for yellow, then the green strip needs to be 0.35 = the ratio 0.26/0.74.
+  const byte greenYellowFraction = 35;
 
   // colors can be set independently.
   byte maxRed = 0;
@@ -386,16 +369,42 @@ void performInstruction() {
 
 }
 
+// from: https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
+#define gC(a) (pgm_read_word_near(gamma + a)) // use lookup table.
+const uint8_t PROGMEM gamma[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+  
+// minimum value on the LEDs that don't flicker
+#define MIN_PWM 3
+
 void lightSet(int lightPin, byte lightLevel) {
 
+  byte cLightLevel = gC(lightLevel); // gamma correction applied by lookup table
+  
   //  Serial << "setting pin " << lightPin << " to " << lightLevel << endl;
-  if ( lightLevel <= 0 ) { // special case for fireLevel==0 i.e. "none"
+  if ( cLightLevel <= 0 ) { // special case for fireLevel==0 i.e. "none"
     digitalWrite(lightPin, LOW); // also, faster than analogWrite
-  } else if ( lightLevel >= 255 ) { // special case for fireLevel==255 i.e. "full"
+  } else if ( cLightLevel >= 255 ) { // special case for fireLevel==255 i.e. "full"
     digitalWrite(lightPin, HIGH); // also, faster than analogWrite
   } else {
     // remap to scootch up to minimum before flicker.
-    analogWrite(lightPin, max(MIN_PWM, lightLevel)); // analogWrite for everything between
+//    analogWrite(lightPin, max(MIN_PWM, lightLevel)); // analogWrite for everything between
+    analogWrite(lightPin, cLightLevel); // analogWrite for everything between
   }
 }
 
@@ -419,10 +428,14 @@ void flameOn(int fireLevel) {
     flameOnTime.interval(flameTime);
     flameOnTime.reset();
 
-    Serial << F("Flame on!") << endl;
+    // set flame cooldown from config
+    flameCoolDownTime.interval(config.flameCoolDownTime);
 
     // start cooldown counter
     flameCoolDownTime.reset();
+
+    Serial << F("Flame on! ") << fireLevel << F(" mapped [") << config.minFireTime << F(",") << config.maxFireTime << F("]");
+    Serial << F(" -> ") << flameTime << F("ms.  Cooldown: ") << config.flameCoolDownTime << F("ms.") << endl;
   }
 }
 
