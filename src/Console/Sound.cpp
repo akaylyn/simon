5580211e4dad43986adb7755088c06d47fdac4c9
Andrea Pollitt <andrea.pollitt@changepoint.com>
2015-03-30 18:54:28 -0700

    wav-trigger: replace sound files
    - use symlinks for tests

--- src/Console/Sound.cpp
+++ src/Console/Sound.cpp
@@ -1,172 +1,108 @@
 #include "Sound.h"
 
+// instantiate
+Sound sound;
+wavTrigger wav;
+
 bool Sound::begin() {
-  Serial << "Sound: startup." << endl;
 
-  // setup comms to Music
-  Music.begin(MUSIC_COMMS_RATE);
-
-  // setup EasyTransfer
-  Serial << "EasyTransfer: pre-startup." << endl;
-  easyTransfer.begin(details(message), &Music);
-  Serial << "EasyTransfer: startup." << endl;
-
-  // set the default volume
-  setVolume(MUSIC_DEFAULT_VOL);
-  stop();
-
-  // Fx board
-  Serial << F("Fx: setup.") << endl;
-  
-  // setup pins
-  for(int i=0; i<N_TRIGGER; i++) {
-    pinMode(pin[i],OUTPUT);
-    digitalWrite(pin[i], HIGH);
+  // set up comms at 57600 baud 
+  WTSerial.begin(57600);
+  if( !WTSerial ) {
+    Serial << F("Sound: error setting up WAV board serial.") << endl;
+    return( false );
   }
-  // volume settings
-  pinMode(FX_VOL_UP, OUTPUT);
-  digitalWrite(FX_VOL_UP, HIGH);
-  pinMode(FX_VOL_DOWN, OUTPUT);
-  digitalWrite(FX_VOL_UP, HIGH);
-
-  // reset the board
-  fxReset();
-  // turn the volume all the way up  
-  fxVolMax();
-
-  Serial << "Sound: startup complete." << endl; 
-
-  return ( Music ); // if there was an error with Serial connection to Music, return false.
+  
+  // start the wav board
+  wav.start(&WTSerial);
+  
+  // set master gain
+  setMasterGain();
+  
+  return( true );
 }
 
-void Sound::playWin(int playCount) {
-  message.type = TYPE_WIN;
-  message.playCount = playCount;
-  sendData();
+void Sound::setMasterGain(int gain) {
+  wav.masterGain(constrain(gain, -70, 4));
 }
 
-void Sound::playLose() {
-  message.type = TYPE_LOSE;
-  sendData();
+int Sound::playTrack(int track, int gain, bool repeat) {
+  // enforce limits
+  int tr = constrain(track, 1, 999);
+  int ga = constrain(gain, -70, 10);
+
+  // set volume
+  wav.trackGain(tr, ga);
+  // play in polyphonic mode
+  wav.trackPlayPoly(tr);
+  // set looping
+  wav.trackLoop(tr, repeat);
+
+  Serial << F("Sound: starting track:") << tr << F(" gain:") << ga << F(" repeat:") << repeat << endl;
+  
+  return( tr );
 }
 
-void Sound::playBaff() {
-  message.type = TYPE_BAFF;
-  sendData();
+int Sound::playWin(int track, int gain, bool repeat) {
+  return( playTrack(track==RANDOM_TRACK ? randomTrack(trWins) : track, gain, repeat) );
+}
+int Sound::playLose(int track, int gain, bool repeat) {
+  return( playTrack(track==RANDOM_TRACK ? randomTrack(trLose) : track, gain, repeat) );
+}
+int Sound::playBaff(int track, int gain, bool repeat) {
+  return( playTrack(track==RANDOM_TRACK ? randomTrack(trBaff) : track, gain, repeat) );
+}
+int Sound::playRock(int track, int gain, bool repeat) {
+  return( playTrack(track==RANDOM_TRACK ? randomTrack(trRock) : track, gain, repeat) );
+}
+int Sound::randomTrack(const int (&range)[2]) {
+  return( random(range[0], range[1]+1) );
 }
 
-void Sound::playRock(int playCount) {
-  message.type = TYPE_ROCK;
-  message.playCount = playCount;
-  sendData();
-}
-
-void Sound::playTone(byte colorIndex, boolean correctTone) {
+// convenience function for Fx board. returns track #.
+int Sound::playTone(byte colorIndex, boolean correctTone) {
+  int tr;
   if ( correctTone ) {
     switch ( colorIndex ) {
-      case I_RED: fxOn(TR_RED); break;
-      case I_GRN: fxOn(TR_GRN); break;
-      case I_BLU: fxOn(TR_BLU); break;
-      case I_YEL: fxOn(TR_YEL); break;
+      case I_RED: tr=trTones[0]; break;
+      case I_GRN: tr=trTones[1]; break;
+      case I_BLU: tr=trTones[2]; break;
+      case I_YEL: tr=trTones[3]; break;
     }
   } else {
-    fxOn(TR_WRONG);
+    tr=trTones[4];
   }
+  
+  // looping enabled!
+  return( playTrack(tr, DEFAULT_TONE_GAIN, true) );
 }
 
-void Sound::fxReset() {
-  // reset the board
-  digitalWrite(FX_RESET, LOW);
-  delay(FX_PRESS_DELAY);
-  digitalWrite(FX_RESET, HIGH);
-  delay(FX_PRESS_DELAY);
-  Serial << F("Fx: reset.") << endl;
-}
-void Sound::fxOn(Trigger t) {
-  digitalWrite(pin[t], LOW);
-//  Serial << "Fx: " << t << " pin: " << pin[t] << endl;
+
+// Stop a track
+void Sound::stopTrack(int track) {
+ // enforce limits
+  int tr = constrain(track, 1, 999);
+  // stop
+  wav.trackStop(tr);
+  
+  Serial << F("Sound: stopping track:") << tr << endl;
 }
 
-void Sound::fxOff(Trigger t) {
-  digitalWrite(pin[t], HIGH);
+// Stop all track
+void Sound::stopAllTracks() {
+  // stop
+  wav.stopAllTracks();
+  
+  Serial << F("Sound: stopping all tracks:") << endl;
 }
 
-void Sound::fxAllOff() {
-  // all off
-  for(int i=0; i<N_TRIGGER; i++) {
-    digitalWrite(pin[i], HIGH);
-  }
+// Fade out track
+void Sound::fadeTrack(int track, unsigned long fadeTime) {
+  // enforce limits
+  int tr = constrain(track, 1, 999);
+  // fade, with Stop at the end.  Stop is important, so voices can be freed up.
+  wav.trackFade(tr, -70, fadeTime, true);
+  
+  Serial << F("Sound: fading track:") << tr << F(" stopped in:") << fadeTime << endl;
 }
 
-void Sound::fxVolUp() {
-  // make sure DOWN isn't pegged
-  digitalWrite(FX_VOL_DOWN, HIGH);
-
-  // toggle the down pin
-  digitalWrite(FX_VOL_UP, LOW);
-  delay(FX_PRESS_DELAY);
-  digitalWrite(FX_VOL_UP, HIGH);  
-  delay(FX_PRESS_DELAY);
-}
-void Sound::fxVolDown() {
-  // make sure UP isn't pegged
-  digitalWrite(FX_VOL_UP, HIGH); 
-
-  // toggle the down pin
-  digitalWrite(FX_VOL_DOWN, LOW);
-  delay(FX_PRESS_DELAY);
-  digitalWrite(FX_VOL_DOWN, HIGH);  
-  delay(FX_PRESS_DELAY);
-}
-void Sound::fxVolMax() {
-  // make sure DOWN isn't pegged
-  digitalWrite(FX_VOL_DOWN, HIGH); 
-
-  // leave the pin low to get +8dB/sec
-  digitalWrite(FX_VOL_UP, LOW);
-
-  Serial << F("Fx: maximum volume.") << endl;
-}
-void Sound::fxVolMin() {
-  // make sure UP isn't pegged
-  digitalWrite(FX_VOL_UP, HIGH); 
-
-  // leave the pin low to get -8dB/sec
-  digitalWrite(FX_VOL_DOWN, LOW);
-
-  Serial << F("Fx: minimum volume.") << endl;
-}
-
-void Sound::setVolume(int level) {
-  char send = '0' + constrain(level, MUSIC_MIN_VOL, MUSIC_MAX_VOL);
-  volume = send - '0';
-  message.volume = send;
-  sendData();
-}
-
-void Sound::incVolume() {
-  setVolume(volume++);
-}
-
-void Sound::decVolume() {
-  setVolume(volume--);
-}
-
-void Sound::stop() {
-//  noTone(SPEAKER_WIRE);
-  fxAllOff();
- 
-  message.type = TYPE_STOP;
-  sendData();
-}
-
-void Sound::sendData() {
-  easyTransfer.sendData();
-
-  // reset message parameters
-  // not every usage of the message structure sets all values
-  message.type = TYPE_DEFAULT;
-  message.playCount = PLAY_COUNT_DEFAULT;
-}
-
-Sound sound;
