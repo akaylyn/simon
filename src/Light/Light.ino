fe054026173b5618e80375c993c274aa6fc5c5d3
Andrea Pollitt <andrea.pollitt@changepoint.com>
2015-04-11 12:58:30 -0700

    configure 3x high rim

--- src/Light/Light.ino
+++ src/Light/Light.ino
@@ -7,110 +7,7 @@
 
 // have: 2K memory, 3 bytes memory per LED, 60 LEDs per meter.
 // so, we can support 2000/3/60=11.1 meters, other memory usage notwithstanding.
-
-#include <Adafruit_NeoPixel.h>
-// TODO: move to NeoMatrix to take advantage of the 3x stacked strips
-#include <Streaming.h>
-#include <Metro.h>
-#include <EasyTransfer.h>
-#include <LightMessage.h> // common message definition
-
-// watchdog timer
-#include <avr/wdt.h>
-
-// RIM of LEDs
-#define RIM_PIN 3 // wire to rim DI pin.  Include a 330 Ohm resistor in series.
-// geometry
-#define RIM_N 108*3 // best if divisible by 4
-#define RIM_SEG_LENGTH 27 // floor(RIM_N/4)=27
-#define YEL_SEG_START 12 // start yellow at this pixel
-#define BLU_SEG_START YEL_SEG_START+RIM_SEG_LENGTH
-#define RED_SEG_START BLU_SEG_START+RIM_SEG_LENGTH
-#define GRN_SEG_START RED_SEG_START+RIM_SEG_LENGTH
-
-// 4x touch lighting strips
-#define RED_PIN 4 // wire to button DI pin.  Include a 330 Ohm resistor in series.
-#define GRN_PIN 5 // wire to button DI pin.  Include a 330 Ohm resistor in series.
-#define BLU_PIN 6 // wire to button DI pin.  Include a 330 Ohm resistor in series.
-#define YEL_PIN 7 // wire to button DI pin.  Include a 330 Ohm resistor in series.
-// geometry
-#define BUTTON_N 49 // wrapped around each button
-
-// 1x chotsky lighting
-#define MIDDLE_PIN 8 //
-// geometry
-#define MIDDLE_N 18 // wrapped around middle chotsky
-
-// LED indicator to ack button presses
-#define LED_PIN 13
-
-// button pins.  wire to Mega GPIO, bring LOW to indicate pressed.
-//create object
-EasyTransfer ET;
-
-//give a name to the group of data
-LightET lightInst;
-
-// communications with Console module via Serial port
-#define LightComms Serial1
-#define LIGHT_COMMS_RATE 19200
-
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-
-// strip around the inner rim
-Adafruit_NeoPixel rimJob = Adafruit_NeoPixel(RIM_N, RIM_PIN, NEO_GRB + NEO_KHZ800);
-
-// strips around the buttons
-Adafruit_NeoPixel redL = Adafruit_NeoPixel(BUTTON_N, RED_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel grnL = Adafruit_NeoPixel(BUTTON_N, GRN_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel bluL = Adafruit_NeoPixel(BUTTON_N, BLU_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel yelL = Adafruit_NeoPixel(BUTTON_N, YEL_PIN, NEO_GRB + NEO_KHZ800);
-
-// strip around the middle chotsky
-Adafruit_NeoPixel midL = Adafruit_NeoPixel(MIDDLE_N, MIDDLE_PIN, NEO_GRB + NEO_KHZ800);
-
-// LED brightness is not equivalent across colors.  Higher wavelengths are dimmed to balance.
-#define RED_MAX 255
-#define GRN_MAX 220
-#define BLU_MAX 210
-
-#define LED_OFF 0 // makes it a litle brighter in the Console.  0 is fine, too.
-// define some colors
-const uint32_t SweetLoveMakin = rimJob.Color(RED_MAX / 4, GRN_MAX / 6, BLU_MAX / 9);
-const uint32_t Red = rimJob.Color(RED_MAX, LED_OFF, LED_OFF);
-const uint32_t Yel = rimJob.Color(RED_MAX, GRN_MAX, LED_OFF);
-const uint32_t Grn = rimJob.Color(LED_OFF, GRN_MAX, LED_OFF);
-const uint32_t Blu = rimJob.Color(LED_OFF, LED_OFF, BLU_MAX);
-const uint32_t Dead = rimJob.Color(LED_OFF, LED_OFF, LED_OFF);
-
-// track when we need to send update.
-boolean rimUpdated = false;
-boolean redUpdated = false;
-boolean grnUpdated = false;
-boolean bluUpdated = false;
-boolean yelUpdated = false;
-boolean midUpdated = false;
-
-// reduce intensity at each update, so this is the amount of time we can expect a pixel to last
-#define PIXEL_TTL 3000UL
-
-// since the pixels have a TTL, we need to add some when there's nothing going on.
-#define STRIP_ADD_PIXEL PIXEL_TTL/3
-Metro quietUpdateInterval(STRIP_ADD_PIXEL);
-
-// update the automata on the rim at this interval
-#define STRIP_UPDATE 100UL
-Metro stripUpdateInterval(STRIP_UPDATE);
-
-// count memory usage for LEDs, which is reported at startup.
-#define TOTAL_LED_MEM (RIM_N + BUTTON_N*4 + MIDDLE_N)*3
-
+#include "Light.h"
 void setup() {
   Serial.begin(115200);
 
@@ -131,7 +28,7 @@ void setup() {
   }
   Serial << endl;
   sendHandshake();
-  
+
   Serial << F("Console checked in.  Proceeding...") << endl;
 
   // use WDT to reboot if we hang.
--- tests/Light/ButtonTest/ButtonTest.ino
+++ tests/Light/ButtonTest/ButtonTest.ino
@@ -1,6 +1,20 @@
-#include <Adafruit_NeoPixel.h>
 
-#define PIN 3
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_NeoMatrix.h>
+#include "Light.h"
+
+// TODO: move to NeoMatrix to take advantage of the 3x stacked strips
+#include <Streaming.h>
+#include <Metro.h>
+#include <EasyTransfer.h>
+#include <LightMessage.h> // common message definition
+
+// watchdog timer
+#include <avr/wdt.h>
+
+
+#define PIN 4
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -9,7 +23,8 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(49, PIN, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip = Adafruit_NeoPixel(49, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoMatrix strip = rimJob;
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -17,107 +32,123 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(49, PIN, NEO_GRB + NEO_KHZ800);
 // on a live circuit...if you must, connect GND first.
 
 void setup() {
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+    rimJob.begin();
+    rimJob.show();
+    rimJob.setTextWrap(true);
+    rimJob.Color(255, 0, 0);
+    //strip.begin();
+    //strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-  // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  // Send a theater pixel chase in...
-  theaterChase(strip.Color(127, 127, 127), 50); // White
-  theaterChase(strip.Color(127,   0,   0), 50); // Red
-  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
 
-  rainbow(20);
-  rainbowCycle(20);
-  theaterChaseRainbow(50);
+    // Some example procedures showing how to display to the pixels:
+    colorWipeMatrix(rimJob.Color(255, 0, 0), 30); // Red
+    colorWipeMatrix(rimJob.Color(0, 255, 0), 30); // Green
+    colorWipeMatrix(rimJob.Color(0, 0, 255), 30); // Blue
+    // Send a theater pixel chase in...
+    /*
+    theaterChase(strip.Color(127, 127, 127), 50); // White
+    theaterChase(strip.Color(127,   0,   0), 50); // Red
+    theaterChase(strip.Color(  0,   0, 127), 50); // Blue
+
+    rainbow(20);
+    rainbowCycle(20);
+    theaterChaseRainbow(50);
+    */
+}
+void colorWipeMatrix(uint32_t c, uint8_t wait) {
+    for(uint16_t x=0; x<108; x++) {
+        strip.drawPixel(x, 0, c);
+        strip.drawPixel(x, 1, c);
+        strip.drawPixel(x, 2, c);
+        strip.show();
+        delay(wait);
+    }
 }
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
-  }
+    for(uint16_t i=0; i<strip.numPixels(); i++) {
+        strip.setPixelColor(i, c);
+        strip.show();
+        delay(wait);
+    }
 }
 
 void rainbow(uint8_t wait) {
-  uint16_t i, j;
+    uint16_t i, j;
 
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+    for(j=0; j<256; j++) {
+        for(i=0; i<strip.numPixels(); i++) {
+            strip.setPixelColor(i, Wheel((i+j) & 255));
+        }
+        strip.show();
+        delay(wait);
     }
-    strip.show();
-    delay(wait);
-  }
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
+    uint16_t i, j;
 
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+        for(i=0; i< strip.numPixels(); i++) {
+            strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+        }
+        strip.show();
+        delay(wait);
     }
-    strip.show();
-    delay(wait);
-  }
 }
 
 //Theatre-style crawling lights.
 void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-     
-      delay(wait);
-     
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
+    for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+        for (int q=0; q < 3; q++) {
+            for (int i=0; i < strip.numPixels(); i=i+3) {
+                strip.setPixelColor(i+q, c);    //turn every third pixel on
+            }
+            strip.show();
+
+            delay(wait);
+
+            for (int i=0; i < strip.numPixels(); i=i+3) {
+                strip.setPixelColor(i+q, 0);        //turn every third pixel off
+            }
+        }
     }
-  }
 }
 
 //Theatre-style crawling lights with rainbow effect
 void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-        }
-        strip.show();
-       
-        delay(wait);
-       
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, 0);        //turn every third pixel off
+    for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+        for (int q=0; q < 3; q++) {
+            for (int i=0; i < strip.numPixels(); i=i+3) {
+                strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+            }
+            strip.show();
+
+            delay(wait);
+
+            for (int i=0; i < strip.numPixels(); i=i+3) {
+                strip.setPixelColor(i+q, 0);        //turn every third pixel off
+            }
         }
     }
-  }
 }
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else if(WheelPos < 170) {
-    WheelPos -= 85;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  }
+    WheelPos = 255 - WheelPos;
+    if(WheelPos < 85) {
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    } else if(WheelPos < 170) {
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    } else {
+        WheelPos -= 170;
+        return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    }
 }
 
