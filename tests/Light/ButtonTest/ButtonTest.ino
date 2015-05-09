85c83f583ed6ce6edba0bbeb781492ee76ebe09e
Andrea Pollitt <andrea.pollitt@changepoint.com>
2015-05-09 14:37:51 -0700

    fix indents

--- tests/Light/ButtonTest/ButtonTest.ino
+++ tests/Light/ButtonTest/ButtonTest.ino
@@ -29,10 +29,10 @@ Adafruit_NeoMatrix strip = rimJob;
 // on a live circuit...if you must, connect GND first.
 
 void setStripColor(Adafruit_NeoPixel &strip, int r, int g, int b) {
-    for (int i = 0; i < strip.numPixels(); i++) {
-        strip.setPixelColor(i, strip.Color(r, g, b));
-    }
-    strip.show();
+  for (int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, strip.Color(r, g, b));
+  }
+  strip.show();
 }
 
 ConcurrentAnimator animator;
@@ -47,179 +47,179 @@ LaserWipePosition laserPos;
 
 void setup() {
 
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    // Colors
-    red.red = RED_MAX;
-    red.blue = LED_OFF;
-    red.green = LED_OFF;
+  // Colors
+  red.red = RED_MAX;
+  red.blue = LED_OFF;
+  red.green = LED_OFF;
 
-    // Neopixel strips
-    rimJob.begin();
-    rimJob.show();
+  // Neopixel strips
+  rimJob.begin();
+  rimJob.show();
 
-    rimConfig.name = "Outer rim";
-    rimConfig.strip = &strip;
-    rimConfig.color = red;
-    rimConfig.ready = true;
-    rimConfig.position = 0;
-    rimConfig.timer = Metro(10);
+  rimConfig.name = "Outer rim";
+  rimConfig.strip = &strip;
+  rimConfig.color = red;
+  rimConfig.ready = true;
+  rimConfig.position = 0;
+  rimConfig.timer = Metro(10);
 
-    redL.begin();
-    redL.show();
-    redButtonConfig.name = "Red Button";
-    redButtonConfig.strip = &redL;
-    redButtonConfig.color = red;
-    redButtonConfig.ready = true;
-    redButtonConfig.position = &laserPos;
-    redButtonConfig.timer = Metro(10);
+  redL.begin();
+  redL.show();
+  redButtonConfig.name = "Red Button";
+  redButtonConfig.strip = &redL;
+  redButtonConfig.color = red;
+  redButtonConfig.ready = true;
+  redButtonConfig.position = &laserPos;
+  redButtonConfig.timer = Metro(10);
 }
 
 /******************************************************************************
  * Main Loop
-******************************************************************************/
+ ******************************************************************************/
 void loop() {
-    if (stripUpdateInterval.check()) {
-        //animator.animate(wipeStrip, rimConfig);
-        animator.animate(laserStrip, redButtonConfig);
-        stripUpdateInterval.reset();
-    }
+  if (stripUpdateInterval.check()) {
+    //animator.animate(wipeStrip, rimConfig);
+    animator.animate(laserStrip, redButtonConfig);
+    stripUpdateInterval.reset();
+  }
 }
 
 void colorWipeMatrix(Adafruit_NeoMatrix &matrix, int c, int wait) {
-    for (uint16_t x = 0; x < matrix.width(); x++) {
-        matrix.drawPixel(x, 0, c);
-        matrix.drawPixel(x, 1, c);
-        matrix.drawPixel(x, 2, c);
-        matrix.show();
-    }
+  for (uint16_t x = 0; x < matrix.width(); x++) {
+    matrix.drawPixel(x, 0, c);
+    matrix.drawPixel(x, 1, c);
+    matrix.drawPixel(x, 2, c);
+    matrix.show();
+  }
 }
 
 // Fill the dots one after the other with a color
 // returns the position of the led that was lit
 void colorWipe(Adafruit_NeoPixel &strip, int r, int g, int b, void *posData) {
-    int* pos = (int*) posData;
-    int next = (*pos);
+  int* pos = (int*) posData;
+  int next = (*pos);
 
-    if (next > strip.numPixels()) {
-        next = 0;
-    } else {
-        ++next;
-    }
+  if (next > strip.numPixels()) {
+    next = 0;
+  } else {
+    ++next;
+  }
 
-    strip.setPixelColor(next, strip.Color(r, g, b));
-    Serial << F("Set pixel: ") << next << endl;
-    posData = (void*) next;
+  strip.setPixelColor(next, strip.Color(r, g, b));
+  Serial << F("Set pixel: ") << next << endl;
+  posData = (void*) next;
 }
 
 // color wipes the last 8 pixels
 void laserWipe(Adafruit_NeoPixel &strip, int r, int g, int b, void *posData) {
-    LaserWipePosition* pos = static_cast<LaserWipePosition*>(posData);
+  LaserWipePosition* pos = static_cast<LaserWipePosition*>(posData);
 
-    // next is relative to the previous position
-    int next = pos->prev;
-    int end = strip.numPixels() - 1;
-    int start = end - 3;
+  // next is relative to the previous position
+  int next = pos->prev;
+  int end = strip.numPixels() - 1;
+  int start = end - 3;
 
-    // first pixel on, direction set
-    if (pos->prev == 0) {
-        next = start;
-    }
+  // first pixel on, direction set
+  if (pos->prev == 0) {
+    next = start;
+  }
 
-    // reverse direction if at an edge
-    if (pos->prev == end) {
-        pos->dir = 1;
-    }
-    else if (pos->prev == start) {
-        pos->dir = 0;
-    }
+  // reverse direction if at an edge
+  if (pos->prev == end) {
+    pos->dir = 1;
+  }
+  else if (pos->prev == start) {
+    pos->dir = 0;
+  }
 
-    // proceed to the next position
-    if (pos->dir == 1) {
-        --next;
-    } else {
-        ++next;
-    }
+  // proceed to the next position
+  if (pos->dir == 1) {
+    --next;
+  } else {
+    ++next;
+  }
 
-    // clear out the last color and set the next one
-    strip.setPixelColor(pos->prev, strip.Color(LED_OFF, LED_OFF, LED_OFF));
-    strip.setPixelColor(next, strip.Color(r, g, b));
-    pos->prev = next;
+  // clear out the last color and set the next one
+  strip.setPixelColor(pos->prev, strip.Color(LED_OFF, LED_OFF, LED_OFF));
+  strip.setPixelColor(next, strip.Color(r, g, b));
+  pos->prev = next;
 }
 
 void rainbow(uint8_t wait) {
-    uint16_t i, j;
+  uint16_t i, j;
 
-    for(j=0; j<256; j++) {
-        for(i=0; i<strip.numPixels(); i++) {
-            strip.setPixelColor(i, Wheel((i+j) & 255));
-        }
-        strip.show();
-        delay(wait);
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
     }
+    strip.show();
+    delay(wait);
+  }
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
-    uint16_t i, j;
+  uint16_t i, j;
 
-    for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-        for(i=0; i< strip.numPixels(); i++) {
-            strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-        }
-        strip.show();
-        delay(wait);
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
+    strip.show();
+    delay(wait);
+  }
 }
 
 //Theatre-style crawling lights.
 void theaterChase(uint32_t c, uint8_t wait) {
-    for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-        for (int q=0; q < 3; q++) {
-            for (int i=0; i < strip.numPixels(); i=i+3) {
-                strip.setPixelColor(i+q, c);    //turn every third pixel on
-            }
-            strip.show();
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
 
-            delay(wait);
+      delay(wait);
 
-            for (int i=0; i < strip.numPixels(); i=i+3) {
-                strip.setPixelColor(i+q, 0);        //turn every third pixel off
-            }
-        }
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
     }
+  }
 }
 
 //Theatre-style crawling lights with rainbow effect
 void theaterChaseRainbow(uint8_t wait) {
-    for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-        for (int q=0; q < 3; q++) {
-            for (int i=0; i < strip.numPixels(); i=i+3) {
-                strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-            }
-            strip.show();
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
 
-            delay(wait);
+      delay(wait);
 
-            for (int i=0; i < strip.numPixels(); i=i+3) {
-                strip.setPixelColor(i+q, 0);        //turn every third pixel off
-            }
-        }
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
     }
+  }
 }
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
-    WheelPos = 255 - WheelPos;
-    if(WheelPos < 85) {
-        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-    } else if(WheelPos < 170) {
-        WheelPos -= 85;
-        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-    } else {
-        WheelPos -= 170;
-        return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-    }
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+    WheelPos -= 170;
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
 }
 
