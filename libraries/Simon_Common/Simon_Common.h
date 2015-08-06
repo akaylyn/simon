d0f5d37d874c6ad7140fc92619714bc89d810620
the-magister <magister@the-magister.com>
2015-08-06 22:49:04 +0000

    Flagged yellow color defn

--- libraries/Simon_Common/Simon_Common.h
+++ libraries/Simon_Common/Simon_Common.h
@@ -108,7 +108,9 @@ const colorInstruction cOff = {0, 0, 0};
 const colorInstruction cRed = {255, 0, 0};
 const colorInstruction cGreen = {0, 255, 0};
 const colorInstruction cBlue = {0, 0, 255};
-const colorInstruction cYellow = {255, 255, 0};
+// MGD: Tower does pay attention to this color spec, but Light does not.
+//const colorInstruction cYellow = {255, 255, 0};
+const colorInstruction cYellow = {255, 220, 0};
 const colorInstruction cWhite = {255, 255, 255};
 // and this serves as an easy way to pull out the right RGB color from the
 const colorInstruction cMap[N_COLORS] = {cRed, cGreen, cBlue, cYellow};
